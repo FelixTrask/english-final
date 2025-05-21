d3bbbdbd879d9d37c6e68495927dc70a04f77880
FelixTrask <felixtrask@gmail.com>
2025-05-21 08:50:03 -0400

    Updated pages

---
+++ src/pages/CreativePiece.js
@@ -0,0 +1,12 @@
+function CreativePiece() {
+  return (
+    <div className="p-8 max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">Creative Piece</h1>
+      <p className="text-gray-700">
+        intro
+      </p>
+    </div>
+  );
+}
+
+export default CreativePiece;
---
+++ src/pages/MapPage.js
@@ -0,0 +1,12 @@
+function MapPage() {
+  return (
+    <div className="p-8 max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">Map Page</h1>
+      <p className="text-gray-700">
+        intro
+      </p>
+    </div>
+  );
+}
+
+export default MapPage;
